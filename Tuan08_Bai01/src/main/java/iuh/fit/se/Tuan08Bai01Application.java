8dc48e1c143cc1992531421c76ca45332cf7c037
NguyenSinh1902 <lenguyensinh1902@gmail.com>
2025-10-19 14:31:59 +0700

    Bai01

---
+++ Tuan08_Bai01/src/main/java/iuh/fit/se/Tuan08Bai01Application.java
@@ -0,0 +1,13 @@
+package iuh.fit.se;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tuan08Bai01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tuan08Bai01Application.class, args);
+    }
+
+}
